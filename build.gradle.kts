ff6a92d93813276abe45fc6f7ba86a2214ac2a97
theplay3r <theplay3rko@gmail.com>
2021-09-17 23:27:09 +0200

    feat: add maven-publish gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.5.10"
     id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("maven-publish")
 }
 
 group = "me.pljr"
